709aa753994f7fb1755536b7106129c75cb09190
itstarzan <rextzan17@gmail.com>
2022-01-19 09:10:53 +0000

    laurel_sprout:OctaviOs Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_laurel_sprout.mk
+    $(LOCAL_DIR)/octavi_laurel_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_laurel_sprout-user \
-    evolution_laurel_sprout-userdebug \
-    evolution_laurel_sprout-eng
+    octavi_laurel_sprout-user \
+    octavi_laurel_sprout-userdebug \
+    octavi_laurel_sprout-eng
