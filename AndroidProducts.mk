45389795825f3acb758ae181fb195fe596fca755
Musical143143 <basub4615@gmail.com>
2022-03-30 11:53:21 +0530

    awaken maintainer add

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_laurel_sprout.mk
+    $(LOCAL_DIR)/awaken_laurel_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_laurel_sprout-user \
-    octavi_laurel_sprout-userdebug \
-    octavi_laurel_sprout-eng
+    awaken_laurel_sprout-user \
+    awaken_laurel_sprout-userdebug \
+    awaken_laurel_sprout-eng
